b3077a216cd87098fa020e5563005e82cfe1c1e2
josue1dario2 <josue1dario2solis3@gmail.com>
2021-10-22 20:20:15 -0300

    initial commit

---
+++ android/app/src/main/kotlin/com/example/proyect_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.proyect_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/pages/home_temp.dart
@@ -0,0 +1,61 @@
+
+import 'package:flutter/material.dart';
+
+class HomePageTemp extends StatelessWidget{
+
+  final opciones = ['uno','dos','tres','cuatro','cinco','seis','siete','ocho','nueve','diez',
+  'once','doce','trece','catorce','quince','dieciséis','diecisiete','dieciocho','diecinueve','veinte'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes Temp'),
+      ),
+      body: ListView(
+        //children: _crearItems()
+          children: _crearItemsCorto()
+     ),
+    );
+  }
+
+  List<Widget> _crearItems(){
+
+    List<Widget> lista = <Widget>[];
+
+    for (String opt in opciones){
+
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+
+      lista..add(tempWidget)
+           ..add(Divider());
+    }
+
+    return lista;
+  }
+
+  List<Widget> _crearItemsCorto(){
+
+    return opciones.map((item){
+
+    return Column(
+      children: <Widget>[
+        ListTile(
+          title: Text(item +'!'),
+          subtitle: Text('Cualquier cosa'),
+          leading: Icon(Icons.account_box),
+          trailing: Icon(Icons.arrow_forward),
+          onTap: (){},
+        ),
+        Divider()
+      ],
+    );
+
+    }
+    ).toList();
+
+  }
+
+}
