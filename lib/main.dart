08c22930f3a77350d574373169a41d33dde10676
josue1dario2 <josue1dario2solis3@gmail.com>
2021-10-22 20:41:18 -0300

    create home_page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:proyect_1/src/pages/home_temp.dart';
+import 'package:proyect_1/src/pages/home_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget{
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title:'Componentes App',
-      home: HomePageTemp()
+      home: HomePage()
     );
   }
 }
