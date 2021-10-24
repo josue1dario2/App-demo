84dec7e938b63ddb6183ee966d53c102dd9d2809
josue1dario2 <josue1dario2solis3@gmail.com>
2021-10-24 01:52:12 -0300

    create widget alert

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -2,20 +2,75 @@
 import 'package:flutter/material.dart';
 
 class AlertPage extends StatelessWidget{
+  const AlertPage({Key? key}) : super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Alert Page'),
+        title: const Text('Alert Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => _mostrarAlert(context),
+          style:ButtonStyle(
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              )
+            )
+          ),
+          child: const Text('Mostrar alerta'),
+        ),
+
       ),
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.arrow_back_ios),
+        child: const Icon(Icons.arrow_back_ios),
         onPressed: (){
           Navigator.pop(context);
         },
       ),
     );
   }
+
+  void _mostrarAlert(BuildContext context){
+
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context){
+
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          title: const Text('Título'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              Text('Este es el contenido de la caja de alerta'),
+              FlutterLogo(size: 50.0)
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'Cancel'),
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'OK'),
+              child: const Text('OK'),
+            ),
+          ],
+        );
+
+
+      }
+
+    );
+
+  }
+
 }
